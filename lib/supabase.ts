7bc65b7eb0cc49582122483cb5b00959a3c7e224
berrypizza <kkh0510ks@gmail.com>
2026-03-09 16:50:06 +0900

    eeff

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -7,7 +7,14 @@ export function getSupabase() {
     client = createClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      { auth: { flowType: "implicit", persistSession: true } },
+      {
+        auth: {
+          flowType: "implicit",
+          detectSessionInUrl: true,
+          persistSession: true,
+          autoRefreshToken: true,
+        },
+      },
     );
   }
   return client;
