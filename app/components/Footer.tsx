25cec2f5dd5eefc633af83c06400f91de287331e
berrypizza <kkh0510ks@gmail.com>
2026-03-17 19:02:39 +0900

    a

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -55,7 +55,7 @@ export default function Footer() {
               ▶ 유튜브 채널
             </a>
             <a
-              href="/app/privacy"
+              href="/privacy"
               className="hover:opacity-80 transition-opacity"
               style={{ color: "#444" }}>
               개인정보처리방침
